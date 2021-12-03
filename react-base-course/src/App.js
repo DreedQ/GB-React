cc289dc17be49d446ca6bd39291cb68ade887440
Pavel <gpv86x@gmail.com>
2021-12-03 16:02:31 +0300

    ДЗ №3

--- react-base-course/src/App.js
+++ react-base-course/src/App.js
@@ -1,22 +1,36 @@
 import './App.css';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
+import { nanoid } from 'nanoid';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar';
+import Switch from '@mui/material/Switch';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { createTheme, ThemeProvider, } from '@mui/material/styles';
 
 function App() {
-    const [messageList, setMessageList] = useState([])
+    const [messageList, setMessageList] = useState([]);
+    const [chatList, setChatList] = useState([]);
+    const inputRef = useRef();
 
    const addMessage = (event)=>{
        event.preventDefault();
         const messageBuffer = [...messageList];
         const item={
-            id: messageBuffer.length,
+            id: nanoid(),
           text: event.target[0].value,
           author:'user',
        };
+    //    console.log(item)
        if(event.target[0].value){
            messageBuffer.push(item)
            setMessageList(messageBuffer)
        }
        event.target[0].value = ''
+       console.log(messageList)
     }
 
     useEffect(()=>{
@@ -24,7 +38,7 @@ function App() {
         let index = tempMessageList.length
         if(tempMessageList[0]){ 
             if(tempMessageList[index-1].author !== 'Bot') {
-                tempMessageList.push({id: tempMessageList.length, text:"Wow! You so smart.", author:"Bot",})
+                tempMessageList.push({id: nanoid(), text:"Wow! You so smart.", author:"Bot",})
             }
         } 
         const answerBot = setTimeout(()=> {
@@ -34,14 +48,67 @@ function App() {
         }
     }, [messageList])
 
+    useEffect(()=>{
+        inputRef.current?.focus()
+    }, [messageList])
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                light: '#4fb3bf',
+                main: '#00838f',
+                dark: '#005662',
+                contrastText: '#fff',
+              },
+            secondary: {
+                light: '#ff7961',
+                main: '#f44336',
+                dark: '#ba000d',
+                contrastText: '#000',            
+            },
+          },
+      
+      });
+
     return (
+        <ThemeProvider theme={theme}>
+
         <div className="App">
-            <form onSubmit={addMessage} className="form">
-                <textarea />
-                <p><input  type="submit"/></p>
+
+             <Grid container spacing={1}>
+             <AppBar position="static" color="primary" sx={{
+                    display: 'flex',
+                    p: 3,
+                    m: 1,
+                    height: 50,
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    flexDirection: 'row'
+                    }}>
+                 <span>Messanger</span>
+            </AppBar>
+             <Grid xs={4} item className="box">
+                 <Box  >
+                 <List>
+                    {chatList.map(item => <ListItem>{item.name}</ListItem>)}
+                </List>
+                 </Box>
+             <Box >
+                 <form onSubmit={addMessage} className="form">
+                <TextareaAutosize className="form_text" minRows={5} label="Text your message"  style={{ width: 300}} color="white" ref={inputRef}/>
+
+                <Button className="form_btn" type="submit" variant="contained" endIcon="➢" mt={2}>
+              Send
+             </Button>
             </form>
-            <div className="messages">{messageList.map(item => <div className="messages_each">{messageList.indexOf(item)+1}) <b>{item.author}</b> : {item.text}</div>)}</div >
+                 </Box>
+            </Grid>
+             <Grid item xs={8}>
+             <div className="messages">{messageList.map(item => <div className="messages_each" key={item.id}><b>{item.author}</b> : {item.text}</div>)}</div >
+             </Grid>
+             </Grid>
         </div>
+        </ThemeProvider>
     );
 }
 
